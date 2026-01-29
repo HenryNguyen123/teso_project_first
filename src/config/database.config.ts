66a9a0d1d3b7b934f8b0b0b484d30801b0d1ac4a
Minh Nhat <nhokkudo143@gmail.com>
2026-01-29 12:03:52 +0700

    create config database

---
+++ src/config/database.config.ts
@@ -0,0 +1,21 @@
+import { DataSource } from 'typeorm';
+import 'dotenv/config';
+
+export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async () => {
+      const dataSource = new DataSource({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        synchronize: true,
+      });
+
+      return dataSource.initialize();
+    },
+  },
+];
