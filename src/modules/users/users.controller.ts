bb9a1c0f313e134cc24a9b8915a009b977abd1a6
Minh Nhat <nhokkudo143@gmail.com>
2026-01-29 15:25:50 +0700

    setup module controler

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { UsersService } from 'src/modules/users/users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private userService: UsersService) {}
+  //step 1: create user
+  @Post('create')
+  async create() {}
+  //step 2: read user
+  @Get('read')
+  async read() {}
+  //step 3: update user
+  @Put('update')
+  async update() {}
+  //step 4: delete user
+  @Delete('destroy')
+  async destroy() {}
+}
