c6d260e6f97daf4d568dbf57ee449a3c3ca336ae
Minh Nhat <nhokkudo143@gmail.com>
2026-01-30 16:32:14 +0700

    create guard, auth controller import entity, login service

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,10 +1,17 @@
-import { Body, Controller, Post, Req, Res } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  InternalServerErrorException,
+  Post,
+  Res,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { IResponse } from 'src/common/interfaces/response.interface';
 import { AuthService } from 'src/modules/auth/auth.service';
 import { LoginDto } from 'src/modules/auth/dtos/login.dto';
-import { responseError } from 'src/shared/utils/response.util';
-import type { Response, Request } from 'express';
-// import type { Response, Request } from 'express';
+import { responseSuccess } from 'src/shared/utils/response.util';
+import type { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -13,14 +20,35 @@ export class AuthController {
   async login(
     @Body() body: LoginDto,
     @Res({ passthrough: true }) res: Response,
-    @Req() req: Request,
   ): Promise<IResponse> {
     try {
-      const data = await this.authService.login(body, res, req);
-      return data;
+      const isProd = process.env.NODE_ENV === 'production';
+      const data = await this.authService.login(body);
+      //step: set cookie
+      res.cookie('AUTH', data.accessToken, {
+        httpOnly: true,
+        secure: isProd,
+        sameSite: isProd ? 'none' : 'lax',
+        maxAge: 30 * 60 * 1000,
+        path: '/',
+      });
+      res.cookie('RESET', data.resetToken, {
+        httpOnly: true,
+        secure: isProd,
+        sameSite: isProd ? 'none' : 'lax',
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        path: '/',
+      });
+      return responseSuccess('Login successfuly', 0, data.payload);
     } catch (error) {
       console.log(error);
-      return responseError('Internal server error', -500);
+      if (
+        error instanceof BadRequestException ||
+        error instanceof UnauthorizedException
+      ) {
+        throw error;
+      }
+      throw new InternalServerErrorException();
     }
   }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,37 +1,123 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/database/entities/user.entity';
 import { LoginDto } from 'src/modules/auth/dtos/login.dto';
 import { comparePassword } from 'src/shared/utils/hashPassword.util';
 import { emailRegex } from 'src/shared/utils/regex.util';
-import { responseError, responseSuccess } from 'src/shared/utils/response.util';
 import { Repository } from 'typeorm';
-import { Request, Response } from 'express';
-
+import { JwtService } from '@nestjs/jwt';
+import { PasswordResetToken } from 'src/database/entities/password-reset-token.entity';
+interface IPayloadLogin {
+  email: string;
+  dob: Date;
+  fullName: string;
+  gender: string;
+  avatar: string;
+  role: {
+    name: string;
+    code: string;
+  };
+}
+interface IPayloadJWTLogin {
+  sub: number;
+  roleCode: string;
+  email: string;
+}
+interface IPayloadResstTokenLogin {
+  user: object;
+  token: string;
+  expiresAt: Date;
+  isUsed: boolean;
+}
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
+    @InjectRepository(PasswordResetToken)
+    private resetTokenRepository: Repository<PasswordResetToken>,
+    private jwtService: JwtService,
   ) {}
-  async login(body: LoginDto, res: Response, Req: Request) {
-    try {
-      const pass = body.password;
-      const email = body.email;
-      if (!pass || !email) return responseError('', 1000);
-      if (!emailRegex.test(email)) return responseError('', 1001);
-      //step: check user exist
-      const user = await this.usersRepository.findOne({
-        where: { email: email },
-      });
-      if (!user) return responseError('', 1002);
-      //step: check password
-      const checkPassword = await comparePassword(pass, user.password);
-      if (!checkPassword) return responseError('', 1003);
-      return responseSuccess('', 0, user);
-    } catch (error) {
-      console.log('create user error:', error);
-      return responseError('get accout user fail', 1006);
+  async login(body: LoginDto): Promise<{
+    accessToken: string;
+    resetToken: string;
+    payload: any;
+  }> {
+    const pass: string = body.password?.trim();
+    const email: string = body.email?.trim();
+    const keyAccess = process.env.JWT_SECRET_KEY;
+    const keyReset = process.env.JWT_RESET_KEY;
+    const timeExpire = '30m';
+    const timeExpireReset = '7d';
+    //step: validate input
+    if (!pass || !email) {
+      throw new BadRequestException('Email or password is required');
     }
+    if (!emailRegex.test(email)) {
+      throw new BadRequestException('Invalid email format');
+    }
+    if (!keyAccess) {
+      throw new Error('JWT_SECRET_KEY is not defined');
+    }
+    //step: check user exist
+    const user = await this.usersRepository.findOne({
+      where: { email: email },
+      relations: {
+        role: true,
+      },
+    });
+    if (!user) {
+      throw new UnauthorizedException('User does not exist');
+    }
+    //step: check password
+    const isValid = await comparePassword(pass, user.password);
+    if (!isValid) {
+      throw new UnauthorizedException('Incorrect password');
+    }
+    //step: payload response
+    const payload: IPayloadLogin = {
+      email: user.email,
+      dob: user.dob,
+      fullName: user.fullName,
+      gender: user.gender,
+      avatar: user.avatar,
+      role: {
+        name: user.role.name,
+        code: user.role.code,
+      },
+    };
+    const payloadJWT: IPayloadJWTLogin = {
+      sub: user.id,
+      roleCode: user.role.code,
+      email: user.email,
+    };
+    // step: sign token
+    const accessToken = await this.jwtService.signAsync(payloadJWT, {
+      secret: keyAccess,
+      expiresIn: timeExpire,
+    });
+    const resetToken = await this.jwtService.signAsync(payloadJWT, {
+      secret: keyReset,
+      expiresIn: timeExpireReset,
+    });
+    //step: save reset Token
+    const expiresAt = new Date();
+    expiresAt.setDate(expiresAt.getDate() + 7);
+    const payloadResstToken: IPayloadResstTokenLogin = {
+      user: user,
+      token: resetToken,
+      expiresAt: expiresAt,
+      isUsed: false,
+    };
+    const resetTokenEntity =
+      this.resetTokenRepository.create(payloadResstToken);
+    await this.resetTokenRepository.save(resetTokenEntity);
+    //step: output data
+    const data = { accessToken, resetToken, payload };
+    return data;
   }
 }
