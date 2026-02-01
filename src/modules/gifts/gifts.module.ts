c8585abf57ed3c2e81b5c3cd2f2da0772e9c1369
Minh Nhat <nhokkudo143@gmail.com>
2026-02-01 20:35:35 +0700

    create function all gifts and find gift by id

--- src/modules/gifts/gifts.module.ts
+++ src/modules/gifts/gifts.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { GiftsService } from './gifts.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SystemGift } from 'src/database/entities/system-gift.entity';
+import { UserGift } from 'src/database/entities/user-gift.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([SystemGift, UserGift])],
+  exports: [TypeOrmModule, GiftsService],
   providers: [GiftsService],
 })
 export class GiftsModule {}
--- src/shared/utils/response.util.ts
+++ src/shared/utils/response.util.ts
@@ -1,8 +1,7 @@
 export const responseSuccess = (message: string, code: number, data: any) => ({
   EM: message,
   EC: code,
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  DT: data,
+  DT: data as object | object[],
 });
 
 export const responseError = (message: string, code: number = -1) => ({
